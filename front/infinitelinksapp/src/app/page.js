f942fa646c10683d744081eb081c12b721f41ba8
Mariem BenYounes <maryemby21@gmail.com>
2024-07-26 11:21:03 +0100

    updated stuff

--- front/infinitelinksapp/src/app/page.js
+++ front/infinitelinksapp/src/app/page.js
@@ -1,15 +1,15 @@
 'use client'
 import { useState } from 'react'
 import Head from 'next/head'
-import Navbar from '@/app/components/Navbar'
-import Sidebar from '@/app/components/Sidebar'
-import ProfileWidget from '@/app/components/ProfileWidget'
-import PerformanceWidget from '@/app/components/PerformanceWidget'
-import ActivityWidget from '@/app/components/ActivityWidget'
-import SocialWidget from '@/app/components/SocialWidget'
-import Footer from '@/app/components/Footer'
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
+import ProfileWidget from '@/components/ProfileWidget'
+import PerformanceWidget from '@/components/PerformanceWidget'
+import ActivityWidget from '@/components/ActivityWidget'
+import SocialWidget from '@/components/SocialWidget'
+import Footer from '@/components/Footer'
 import { useWallet } from '@/contexts/WalletContext'
-import LandingPage from '@/app/components/LandingPage'
+import LandingPage from '@/components/LandingPage'
 
 export default function Dashboard({params}) {
   const [sidebarOpen, setSidebarOpen] = useState(false)
@@ -35,7 +35,7 @@ export default function Dashboard({params}) {
 
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-900">
           <div className="container mx-auto px-6 py-8">
-            <h3 className="text-3xl font-medium text-white mb-6">Welcome back, {username}!</h3>
+            <h3 className="text-3xl font-medium text-white mb-6">Welcome!</h3>
             <div className="flex flex-wrap -mx-6">
               <ProfileWidget />
               <PerformanceWidget />
